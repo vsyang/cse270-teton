1e4e48d6a778b7e246325717947a85c52305b175
vsyang <yang.vanessa85@gmail.com>
2025-03-24 21:35:58 -0600

    updated headless

--- cse270/tests/test_smokeTest.py
+++ cse270/tests/test_smokeTest.py
@@ -9,12 +9,15 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Firefox(options=options)
     self.vars = {}
-  
+    
   def teardown_method(self, method):
     self.driver.quit()
   
